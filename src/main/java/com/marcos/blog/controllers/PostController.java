41bdff48f68d08c5f882398b3d20cd7efd38bacd
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-12 10:45:26 +0100

    Created Post controller

---
+++ src/main/java/com/marcos/blog/controllers/PostController.java
@@ -0,0 +1,48 @@
+package com.marcos.blog.controllers;
+
+import com.marcos.blog.models.Post;
+import com.marcos.blog.payload.requests.PostRequest;
+import com.marcos.blog.payload.response.PostResponse;
+import com.marcos.blog.services.PostService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponse> getPostById(@PathVariable UUID id) {
+        return postService.getPostById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<PostResponse> createPost(@RequestBody @Valid PostRequest postRequest) {
+        return postService.createPost(postRequest);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PostResponse> updatePost(@PathVariable UUID id, @Valid @RequestBody PostRequest postRequest) {
+        return postService.updatePost(id, postRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable UUID id) {
+        return postService.deletePost(id);
+    }
+}
