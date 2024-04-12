9b3e9eb9de8daae0b97e8c19794c2ab67d91a69d
FCT-Marcos <164381752+FCT-Marcos@users.noreply.github.com>
2024-04-12 11:01:25 +0100

    Created User request

---
+++ src/main/java/com/marcos/blog/payload/requests/UserRequest.java
@@ -0,0 +1,20 @@
+package com.marcos.blog.payload.requests;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record UserRequest(
+        @NotEmpty()
+        @Size(min = 3, max = 20)
+        String name,
+
+        @NotEmpty
+        @Size(min = 3, max = 20)
+        String password,
+
+        @NotEmpty()
+        @Email()
+        String email
+) {
+}
